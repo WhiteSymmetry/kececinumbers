85d9c6defb5d31e95c9a90d9910c6b4b7a853e3b
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-07-24 14:47:36 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ copyright = '2025, Mehmet Keçeci'
 # Version Management
 # from setuptools_scm import get_version
 # version = get_version(root='..', relative_to=__file__)
-version = '0.3.0'  # Replace with your actual version number
+version = '0.3.1'  # Replace with your actual version number
 release = version
 
 # General Configuration
