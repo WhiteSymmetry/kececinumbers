c59a45ed016d357fccff767486d7bd12c0824867
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-19 20:57:13 +0000

    Update _version.py

--- kececinumbers/_version.py
+++ kececinumbers/_version.py
@@ -1,6 +1,6 @@
 # _version.py
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __license__ = "MIT"
 __description__ = "Keçeci Numbers: An Exploration of a Dynamic Sequence Across Diverse Number Sets."
 __author__ = "Mehmet Keçeci"
