cc36a7fc78a1e3533868bb99de9b79e6a16a49cb
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-19 20:20:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kececinumbers",
-    version="0.3.8",
+    version="0.3.9",
     description="Keçeci Numbers: An Exploration of a Dynamic Sequence Across Diverse Number Sets",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
