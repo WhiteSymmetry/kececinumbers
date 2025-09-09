dd15483a29dc19b7cf59737b35c132338147e94d
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-09-09 11:52:21 +0000

    Update __init__.py

--- kececinumbers/__init__.py
+++ kececinumbers/__init__.py
@@ -22,7 +22,7 @@ import warnings
     # importlib.reload(kececinumbers) # F821 undefined name 'kececinumbers'
 
 # Paket sürüm numarası
-__version__ = "0.6.6"
+__version__ = "0.6.7"
 __author__ = "Mehmet Keçeci"
 __email__ = "mkececi@yaani.com"
 
