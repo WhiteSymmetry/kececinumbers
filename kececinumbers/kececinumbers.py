91e9b9b78837cfdf6f43857df48d947828bb1bed
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-09-12 14:22:03 +0000

    Update kececinumbers.py

--- kececinumbers/kececinumbers.py
+++ kececinumbers/kececinumbers.py
@@ -39,7 +39,7 @@ import numbers
 #from numbers import Real
 import numpy as np
 import quaternion   
-# pip instal numpy-quaternion # conda install -c conda-forge quaternion
+# pip install numpy-quaternion # conda install -c conda-forge quaternion
 import random
 import re
 from scipy.fft import fft, fftfreq
